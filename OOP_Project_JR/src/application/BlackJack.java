5d63e9f9c02a9d19abc62da18104e0599b354d92
jackd <jackd@Jacks_PC.attlocal.net>
2024-11-28 10:56:13 -0500

    Creation of blackjack game window

---
+++ OOP_Project_JR/src/application/BlackJack.java
@@ -0,0 +1,11 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+
+public class BlackJack {
+	
+	@FXML private AnchorPane BJMenu;
+
+}
--- OOP_Project_JR/src/application/MainMenu.java
+++ OOP_Project_JR/src/application/MainMenu.java
@@ -1,15 +1,33 @@
 package application;
 
+import java.io.IOException;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 public class MainMenu {
 	
 	@FXML private AnchorPane MainMenu;
+	@FXML private Button BJMenuBtn;
 	
-	@FXML protected void blackjackStartBtn(ActionEvent event) {
+	@FXML protected void blackjackStartBtn(ActionEvent event) throws IOException {
     	
+		Parent root = FXMLLoader.load(getClass().getResource("BlackJack.fxml"));
+		Scene BlackJack = new Scene(root, 800, 500);
+		String css = this.getClass().getResource("application.css").toExternalForm();
+		BlackJack.getStylesheets().add(css);
+		Stage window =  (Stage)(((Node) event.getSource()).getScene().getWindow());
+		window.setScene(BlackJack);
+		window.setTitle("BlackJack");
+		window.show();
+		
     }
 	
 	@FXML protected void solitareStartBtn(ActionEvent event) {
